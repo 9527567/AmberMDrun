57920d1c184cdc2eaed380beec6d24f2ead4ba41
9527567 <z9527567@gmail.com>
2022-12-05 22:22:11 +0800

    update setup

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import argparse as arg
+import argparse
 import pyamber as pya
 import os
+def main():
+    parser = argparse.ArgumentParser(description='Demo of argparse')
+    parser.add_argument('--epochs', type=int, default=30)
+    parser.add_argument('--batch', type=int, default=4)
+    args = parser.parse_args()
+    print(args)
 if __name__ == '__main__':
-    s = pya.SystemInfo("com.parm7","com.rst7")
-    min1 = pya.Min("step1",systemInfo=s,ref="com.rst7",restraint_wt=5.0)
-    min1.Run()
-    md = pya.Md(name="step2",systemInfo=s,ref="step1.rst7",restraint_wt=2.0,dt=0.001)
-    md.Run()
+    main()
